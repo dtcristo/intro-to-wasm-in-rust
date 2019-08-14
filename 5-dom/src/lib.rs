0d94ec7b700a5ac59c561ef629206b37034df83e
David Cristofaro <david@dtcristo.com>
2019-08-14 13:44:04 +1000

    Restructure repo

--- 5-dom/src/lib.rs
+++ 5-dom/src/lib.rs
@@ -2,6 +2,7 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+use web_sys::{window, HtmlButtonElement};
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -13,11 +14,11 @@ pub fn start() {
 
     let callback = Closure::wrap(Box::new(add_message) as Box<dyn Fn()>);
 
-    let document = web_sys::window().unwrap().document().unwrap();
+    let document = window().unwrap().document().unwrap();
     let button = document
         .create_element("button")
         .unwrap()
-        .dyn_into::<web_sys::HtmlButtonElement>()
+        .dyn_into::<HtmlButtonElement>()
         .unwrap();
     button.set_inner_html("Click me");
     document.body().unwrap().append_child(&button).unwrap();
@@ -27,7 +28,7 @@ pub fn start() {
 }
 
 fn add_message() {
-    let document = web_sys::window().unwrap().document().unwrap();
+    let document = window().unwrap().document().unwrap();
     let element = document.create_element("p").unwrap();
     element.set_inner_html("Hello from Rust closure!");
     document.body().unwrap().append_child(&element).unwrap();
