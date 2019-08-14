50ff532a35e7a36e0fc93b09eaed519297eccd38
David Cristofaro <david@dtcristo.com>
2019-08-14 13:37:15 +1000

    Add button and callback

--- 5/src/lib.rs
+++ 5/src/lib.rs
@@ -1,24 +1,34 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(start)]
-pub fn start() -> Result<(), JsValue> {
+pub fn start() {
     utils::set_panic_hook();
     // panic!("example panic");
 
-    // https://rustwasm.github.io/wasm-bindgen/api/web_sys/
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
+    let callback = Closure::wrap(Box::new(add_message) as Box<dyn Fn()>);
 
-    let element = document.create_element("p")?;
-    element.set_inner_html("Hello from Rust!");
+    let document = web_sys::window().unwrap().document().unwrap();
+    let button = document
+        .create_element("button")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlButtonElement>()
+        .unwrap();
+    button.set_inner_html("Click me");
+    document.body().unwrap().append_child(&button).unwrap();
+    button.set_onclick(Some(callback.as_ref().unchecked_ref()));
 
-    body.append_child(&element)?;
+    callback.forget();
+}
 
-    Ok(())
+fn add_message() {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let element = document.create_element("p").unwrap();
+    element.set_inner_html("Hello from Rust closure!");
+    document.body().unwrap().append_child(&element).unwrap();
 }
